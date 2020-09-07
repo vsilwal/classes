dc9d0390cb80d10321a64e7d23b1b361d7b0a8f0
vsilwal <vipulsilwal@gmail.com>
2020-09-07 16:48:46 +0530

    User input example

--- numerical_methods/lab/matlab_intro/lat_lon_example.m
+++ numerical_methods/lab/matlab_intro/lat_lon_example.m
@@ -1,17 +1,36 @@
+clc
 clear all
-lat = [20:1:30];
-lon = [80:1:90];
 
-% pre-initialize
-lat_mat = zeros(11,11);
-lon_mat = zeros(11,11);
+% variable input method
+input_method = 1;
 
-for ii = 1:length(lat)
-    for jj = 1:length(lon)
-        lat_mat(ii,jj) = lat(ii);
-        lon_mat(ii,jj) = lon(jj);
-    %fprintf(' lat = %.2f lon = %.2f \n', lat(ii),lon(jj))
-    end 
+if input_method == 1
+    % Initialization
+    lat = [20:1:33];
+    lon = [80:1:88];
+else
+    % ALTERNATE: USER INPUT
+    min_lat = input('Minimum latitute: ');
+    max_lat = input('Maximum latitute: ');
+    min_lon = input('Minimum longitude: ');
+    max_lon = input('Maximum longitude: ');
+    dleta_lat = 1;
+    delta_lon = 1;
+    lat = min_lat:delta_lat:max_lat;
+    lon = min_lon:delta_lon:max_lon;
 end
 
+% pre-initialize
+lat_mat = nan(length(lat),length(lon));
+lon_mat = nan(length(lat),length(lon));
+
+[m,n] = size(lon_mat);
+
+for ii = 1:m
+    for jj = 1:n
+        lat_mat(ii,jj) = lat(ii);
+        lon_mat(ii,jj) = lon(jj);
+        fprintf('lat = %d lon = %d \n', lat(ii),lon(jj))
+    end 
+end
 

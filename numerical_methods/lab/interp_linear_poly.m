5a268b1145a4e199b0f20a57faff756a539f213d
vsilwal <vipulsilwal@gmail.com>
2020-09-11 17:08:08 +0530

    Create interp_linear_poly.m

---
+++ numerical_methods/lab/interp_linear_poly.m
@@ -0,0 +1,18 @@
+n = 10;
+
+xi = linspace(-1,1,10);
+yi = [3 2 2 1 0 -3 -4 1 2 4];
+
+plot(xi,yi,'o','MarkerSize',10,'MarkerFaceColor','r','MarkerEdgeColor','k')
+
+%===========================
+x = linspace(-1,1,1000);
+% y = ?
+% Interpolate to find value of y at x using following 
+% interpolation techniques
+
+% 1. midpoint interpolation
+
+% 2. linear interpolation
+
+% 3. polynomial interpolation

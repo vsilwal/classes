e34256ad0a7c2db671c2f3218d40576cec257810
vsilwal <vipulsilwal@gmail.com>
2020-09-11 17:01:02 +0530

    Update add2.m

--- numerical_methods/lab/matlab_intro/add2.m
+++ numerical_methods/lab/matlab_intro/add2.m
@@ -1,12 +1,15 @@
-function c = add2(a,b)
+function [c,d,e,f] = add2(a,b)
 %
-% Perfoms addition of two numbers
+% Perfoms arithmetic operations on two numbers
 
 c = a + b;
+d = a - b;
+e = a/b;
+f = a*b;
 
 %% Example
 if 0
     val1 = 2;
     val2 = 4;
-    out = add2(val1, val2)
+    [out1,out2,out3] = add2(val1, val2)
 end

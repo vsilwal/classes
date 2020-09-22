882d3d796070c33db1f730b3276943720062cb8a
vsilwal <vipulsilwal@gmail.com>
2020-09-22 18:26:56 +0530

    update lagrange and spline interp codes

--- numerical_methods/lab/lagrange.m
+++ numerical_methods/lab/lagrange.m
@@ -1,5 +1,5 @@
 function [val] = lagrange(xi,yi,x)
-%
+% Function for performing lagrange interpolation
 % INPUT
 %   x       interpolation points
 %   f       function values
@@ -22,14 +22,58 @@ for j=1:n
 end
 
 m = length(x);
-val = zeros(m,1); % y at interpolating points x
+val = zeros(m,1);
 %
 %  evaluate the interpolation polynomial at y
 %
 for i = 1: m
     for j=1:n
-        % Calculate Vi(x)
-        % Sum of yj*Vi(x)
-        XXX
+        lj = x(i) - [xi(1:j-1);xi(j+1:n)];
+        val(i) = val(i) + yi(j)*prod(lj)*w(j);
     end
+end
+
+%% Examples
+if 0
+    clear all
+    close all
+    n = 10
+    xi = linspace(-1,1,n);
+    x = linspace(-1,1,1000);
+    
+    if 0
+        % Create Lagrange polynomials
+        for j =1:n
+            yi = zeros(n,1);
+            yi(j) = 1; % L3
+            
+            % lagrange
+            y =lagrange_sol(xi,yi,x);
+            plot(x,y,'Linewidth',2,'Displayname','lagrange');
+        end
+    end
+    
+    %==========================
+    % Test Example
+    yi = [3 2 2 1 0 -3 -4 1 2 4]; % for n=10
+    
+    %plot
+    plot(xi, yi, 'o','MarkerSize',10,'MarkerFaceColor','r','MarkerEdgeColor','k')
+    hold on
+    
+    % lagrange
+    y =lagrange_sol(xi,yi,x);
+    plot(x,y,'Linewidth',2,'Displayname','lagrange');
+    
+    % linear
+    y = interp1(xi,yi,x);
+    plot(x,y,'b','Linewidth',2,'Displayname','linear')
+    
+    % spline
+    y = interp1(xi,yi,x,'spline');
+    plot(x,y,'g','Linewidth',2,'Displayname','in-built cubic spline')
+    
+    legend
+    ax = gca;
+    ax.FontSize = 16;
 end
--- numerical_methods/lab/quad_spline.m
+++ numerical_methods/lab/quad_spline.m
@@ -1,25 +1,101 @@
-x = [0:0.01:1];
+function [y,m,G] = quad_spline(xi,yi,x)
+% Function for performing quadratic spline interpolation
+% 
+%-------------------
+% Total number of points = N
+% Total number of connecting functions = N-1
+% Total number of unknowns = 3(n-1) % for quadratic
+% Total number of unknowns = 4(n-1) % for cubic (BUT NOT SOLVED HERE)
+% 
+% Dimnesions: 
+% G = 3(N-1) x 3(N-1)
+% m = 3(N-1) x 1
+% d = 3(N-1) x 1
+%-------------------
 
-a1 = 0;
-b1 = 2;
-c1 = 0;
+% check length(xi) == length(yi) 
+if length(xi)~=length(yi)
+    error('Length of input X and input Y must be equal');
+end
 
-% a1 x2 + b1 x + c1
+n = length(xi);
 
-y1 = a1 * x.^2 + b1 * x + c1;
+% pre-allocation
+G = zeros(3*(n-1));
+y = zeros(length(x),1);
 
-plot(x,y1)
-hold on
-%-----------------
-x = [1:0.01:2];
+%------------------------------------
+% final data vector of length 3(n-1)
+% constraint 1
+temp = [];
+for ii = 2:n-1
+    temp = XXX;
+end
+temp = [yi(1); temp; yi(end)];
+% constraint 2
+temp = [temp; zeros(n-2,1)];
+% contraint 3
+temp = [temp; 0];
 
-a1 = -1;
-b1 = 4;
-c1 = -1;
+% Final data vector
+d = temp;
 
-% a1 x2 + b1 x + c1
+%----------------------------------
+% Compute G
 
-y2 = a1 * x.^2 + b1 * x + c1;
+for ii = 2:n-1
+    XXX
+    XXX
+end
+XXX
+XXX
 
-plot(x,y2)
+% Constraint 2
+for ii=2:n-1
+    XXX
+end
 
+% Constraint 3
+G(3*(n-1),1) = 1;
+%-----------------------------
+
+% compute coefficients
+m = XXX;
+
+% Compute polynomials 
+count = 1; 
+for ii = 1:length(x)
+    if x(ii) <= xi(count+1)
+        XXX
+    else
+        XXX
+    end
+end
+
+%% =================================================
+% EXAMPLE
+if 0
+    clear all
+    close all
+    n = 10
+    xi = linspace(-1,1,n);
+    x = linspace(-1,1,1000);
+    % Test Example
+    yi = [3 2 2 1 0 -3 -4 1 2 4];
+    %yi = [3 2 2 1 0 -3 -4 1 2 4 3 2 2 1 0 -3 -4 1 2 4]; % for n=10
+    
+    % Function Call
+    [y2,m,G] = quad_spline(xi,yi,x);
+    
+    y3 = interp1(xi,yi,x,'spline');
+    
+     %plot
+    plot(xi, yi, 'o','MarkerSize',10,'MarkerFaceColor','r','MarkerEdgeColor','k')
+    hold on
+    
+    % plot
+    plot(x,y2,'Linewidth',2,'Displayname','quadratic spline');
+    % plot
+    plot(x,y3,'Linewidth',2,'Displayname','in-built cubic spline');
+    legend
+end

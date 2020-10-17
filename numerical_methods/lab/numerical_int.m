a35e75ae1383899406cc81589e46f39960abbe67
vsilwal <vipulsilwal@gmail.com>
2020-10-17 23:55:08 +0530

    update numerical integration

--- numerical_methods/lab/numerical_int.m
+++ numerical_methods/lab/numerical_int.m
@@ -1,91 +1,176 @@
+clc
+close all
+clear all
+
+iex = 2;
+
 %% EXAMPLE 1:  Trapezoidal rule
-
-% Make an example Parabola
-% y = ax^2 + bx + c
-
-% Input coefficients
-a = -1;
-b = 4;
-c = 0;
-
-x = [-1:.1:5];
-
-f = @(a,b,c,x) a*x.^2 + b*x + c;
-
-y = f(a,b,c,x);
-
-% Area using points
-h = 1;
-x1 = 1; x2 = x1 + h; x3 = x1 + 2*h;
-% Y- points 
-y1 = f(a,b,c,x1); y2 = f(a,b,c,x2); y3 = f(a,b,c,x3); 
-
-% Area using TRAPEZOIDAL RULE
-area1 = 1/2 * h * (y1 + 2*y2 + y3); % KEY
-
-% plot
-figure
-plot(x,y,'k','LineWidth',2);
-grid on, hold on
-
-% plot dashed lines
-plot([x1 x1],[0 y1],'--k');
-plot([x2 x2],[0 y2],'--k');
-plot([x3 x3],[0 y3],'--k');
-plot([x1 x2],[y1 y2],'--k');
-plot([x2 x3],[y2 y3],'--k');
-
-% color area under the trapezoid
-area([x1 x2],[y1 y2]);
-area([x2 x3],[y2 y3]);
-
-% plot circluar markers
-plot([x1],[y1],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
-plot([x2],[y2],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
-plot([x3],[y3],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
+if iex ==1
+    % Make an example Parabola
+    % y = ax^2 + bx + c
+    
+    % Input coefficients
+    a = -1;
+    b = 4;
+    c = 0;
+    
+    % Intialize x
+    x = [-1:.01:5];
+    
+    % Initialize function
+    f = @(a,b,c,x) a*x.^2 + b*x + c;
+    
+    % Call the function and find value of Y at X
+    y = f(a,b,c,x);
+    
+    % Discretize X for summation
+    xmin = 0; xmax = 4;
+    Nsamples = 6; % Number of points for dividing x (N-1 trapezoids)
+    x_sum = linspace(xmin,xmax,Nsamples);
+    h = x_sum(2) - x_sum(1);
+    
+    % Y- points at x for summation
+    y_sum = f(a,b,c,x_sum);
+    
+    % Area using TRAPEZOIDAL RULE
+    %area1 = 1/2 * h * (y1 + 2*y2 + y3); % KEY
+    area1 = 1/2 * h * (y(1) + sum(2*y_sum(2:end-1)) + y(end)); % KEY
+    
+    % plot
+    figure
+    plot(x,y,'k','LineWidth',2);
+    grid on, hold on
+    
+    % color area under the trapezoid
+    for ii = 1:length(x_sum)-1
+        area([x_sum(ii) x_sum(ii+1)],[y_sum(ii) y_sum(ii+1)]);
+    end
+    
+    % plot circluar markers
+    for ii = 1:length(x_sum)
+        plot([x_sum(ii)],[y_sum(ii)],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
+    end
+    title(sprintf('Trapezoidal Rule \n %dx^2 + %dx + %d; Area = %0.2f; N = %d',a,b,c,area1,Nsamples));
+end
 
 %% EXAMPLE 2:  Midpoint rule
+if iex ==2
+    % Make an example Parabola
+    % y = ax^2 + bx + c
+    
+    % Input coefficients
+    a = -1;
+    b = 4;
+    c = 0;
+    
+    % Intialize x
+    x = [-1:.01:5];
+    
+    % Initialize function
+    f = @(a,b,c,x) a*x.^2 + b*x + c;
+    
+    % Call the function and find value of Y at X
+    y = f(a,b,c,x);
+    
+    % Discretize X for summation
+    xmin = 0; xmax = 4;
+    Nsamples = 6; % Number of points for dividing x (N-1 rectangles)
+    x_sum = linspace(xmin,xmax,Nsamples);
+    h = x_sum(2) - x_sum(1);
+    
+    % Find mid-points of x
+    x_mid = conv(x_sum,[0.5 0.5],'valid');
+    
+    % Y- points at mid-points for summation
+    y_mid = f(a,b,c,x_mid);
+    
+    % Area using MID-POINT RULE
+    area1 = h * sum(y_mid); % KEY
+    
+    % plot
+    figure
+    plot(x,y,'k','LineWidth',2);
+    grid on, hold on  
+    
+    % color area under the rectangle
+    for ii = 1:length(x_sum)-1
+        area([x_sum(ii) x_sum(ii+1)],[y_mid(ii) y_mid(ii)]);
+    end
+    
+    % plot circluar markers
+    for ii = 1:length(x_mid)
+        plot([x_mid(ii)],[y_mid(ii)],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
+    end
+    title(sprintf('Mid-point Rule \n %dx^2 + %dx + %d; Area = %0.2f; N = %d',a,b,c,area1,Nsamples))
+end
 
-% Make an example Parabola
-% y = ax^2 + bx + c
+%% EXAMPLE 2:  Simpson's Method
+if iex == 3
+    % Input coefficients
+    a = -1;
+    b = 4;
+    c = 0;
+    
+    % Intialize x
+    x = [-1:.01:5];
+    
+    % Initialize function
+    f = @(a,b,c,x) a*x.^2 + b*x + c;
+    
+    % Find y
+    y = f(a,b,c,x);
+    
+    % Discretize X for summation
+    xmin = 0; xmax = 4;
+    
+    % Number of points for dividing x (N-1 rectangles)
+    % even N means odd number of subintervals (simpsons 3/8 rule)
+    % odd N means even number of subintervals (simpsons even rule)
+    Nsamples = 7; 
+    
+    x_sum = linspace(xmin,xmax,Nsamples);
+    h = x_sum(2) - x_sum(1);
+    
+    % Y- points at x for summation
+    y_sum = f(a,b,c,x_sum);
+    
+    % Call simpson function for integration
+    area1 = simpson(x_sum,y_sum);
+    
+    % plot
+    figure
+    plot(x,y,'k','LineWidth',2);
+    grid on, hold on  
 
-% Input coefficients
-a = -1;
-b = 4;
-c = 0;
+    % color area under the curve
+    for ii = 1:length(x_sum)-1
+        idx = x>x_sum(ii)&x<x_sum(ii+1);
+        area(x(idx),y(idx));
+    end
+    
+    % plot circluar markers
+    for ii = 1:length(x_sum)
+        plot([x_sum(ii)],[y_sum(ii)],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
+    end
+    if mod(length(x_sum),2)==1
+        title(sprintf('Simpsons Rule \n %dx^2 + %dx + %d; Area = %0.2f; N = %d',a,b,c,area1,Nsamples));
+    else
+        title(sprintf('Simpsons 3/8 Rule \n %dx^2 + %dx + %d; Area = %0.2f; N = %d',a,b,c,area1,Nsamples));
+    end
+end
 
-x = [-1:.1:5];
-
-f = @(a,b,c,x) a*x.^2 + b*x + c;
-
-y = f(a,b,c,x);
-
-% Area using points
-h = 1;
-x1 = 1; x2 = x1 + h; x3 = x1 + 2*h;
-% Y- points 
-y1 = f(a,b,c,x1); y2 = f(a,b,c,x2); y3 = f(a,b,c,x3); 
-
-% Area using TRAPEZOIDAL RULE
-area1 = 1/2 * h * (y1 + 2*y2 + y3); % KEY
-
-% plot
-figure
-plot(x,y,'k','LineWidth',2);
-grid on, hold on
-
-% plot dashed lines
-plot([x1 x1],[0 y1],'--k');
-plot([x2 x2],[0 y2],'--k');
-plot([x3 x3],[0 y3],'--k');
-plot([x1 x2],[y1 y2],'--k');
-plot([x2 x3],[y2 y3],'--k');
-
-% color area under the trapezoid
-area([x1 x2],[y1 y2]);
-area([x2 x3],[y2 y3]);
-
-% plot circluar markers
-plot([x1],[y1],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
-plot([x2],[y2],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
-plot([x3],[y3],'o','MarkerFaceColor','r','MarkerEdgeColor','k');
+% Function for simpson's integration
+function area1 = simpson(x,y)
+h = x(2)-x(1);
+N = length(x);
+if mod(N,2)==1 
+    % odd number of points (even number of intervals)
+    % See class notes for derivation
+    area1 = (h/3)*(y(1) + sum(4*y(2:2:end-1)) + sum(2*y(3:2:end-2)) + y(end));
+else
+    % even number of points
+    % odd number of intervals (mod(N,2) == 0); % 3/8 rule
+    % https://en.wikipedia.org/wiki/Simpson's_rule#Simpson.27s_3.2F8_rule
+    area1 = (3*h/8)*(y(1) + sum(3*y(2:3:end-2)) + sum(3*y(3:3:end-1)) + sum(2*y(3:3:end-4)) + y(end));
+end
+end
